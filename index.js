b31fcddb007733ecd36d49d88adf3579501fac5e
IamJayashree <71251236+IamJayashree@users.noreply.github.com>
2023-05-11 14:26:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require ('dotenv').config();
 const port = process.env.PORT;
+var cors = require('cors');
+
 
 const app= express();
 console.log(port);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
 app.use('/openai',require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log('server started on port ${port}'));
+
