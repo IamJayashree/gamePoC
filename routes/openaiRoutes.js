c4434efc86891abed7b2bf1e9b92ea571d5b5fbb
IamJayashree <71251236+IamJayashree@users.noreply.github.com>
2023-05-11 14:27:26 +0530

    Update openaiRoutes.js

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const { generateImage }=require('../controllers/openaiController');
+const { generateImage , generateImageModel1 , generateImageModel2, generateImageModel3, generateImageModel4, createImageVariation,createImageVariation1, createImageVariation2, createImageVariation3, createImageVariation4  }=require('../controllers/openaiController');
+//const { generateImage }=require('../controllers/openaiController');
 const router = express.Router();
 
 router.post('/generateimage',generateImage);
+router.post('/generateimage1',generateImageModel1);
+router.post('/generateimage2',generateImageModel2);
+router.post('/generateimage3',generateImageModel3);
+router.post('/generateimage4',generateImageModel4);
+router.post('/imagevariation',createImageVariation);
+router.post('/imagevariation1',createImageVariation1);
+router.post('/imagevariation2',createImageVariation2);
+router.post('/imagevariation3',createImageVariation3);
+router.post('/imagevariation4',createImageVariation4);
 
 module.exports = router;
